a631a22cbc33d9e804949b924d2fdbd78e686d8a
abc <soo990414@edu.dju.ac.kr>
2025-07-24 20:01:38 +0900

    article list

--- src/main/java/com/practice/dockerConn/domain/article/ApiV1ArticleController.java
+++ src/main/java/com/practice/dockerConn/domain/article/ApiV1ArticleController.java
@@ -1,11 +1,23 @@
 package com.practice.dockerConn.domain.article;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/articles")
 public class ApiV1ArticleController {
+    private final ArticleService articleService;
+
+    @GetMapping("")
+    public String readArticles (Model model) {
+        List<Article> articles = this.articleService.getArticles();
+        model.addAttribute(articles);
+        return "article_list";
+    }
 }
--- src/main/java/com/practice/dockerConn/domain/article/ArticleService.java
+++ src/main/java/com/practice/dockerConn/domain/article/ArticleService.java
@@ -4,8 +4,16 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class ArticleService {
+    private final ArtilceRepository artilceRepository;
+
+    public List<Article> getArticles () {
+        List<Article> articles = this.artilceRepository.findAll();
+        return articles;
+    }
 }
