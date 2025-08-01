441060b56337084c5a9c6d094337b370fedd9a58
abc <soo990414@edu.dju.ac.kr>
2025-08-01 20:31:19 +0900

    게시글 생성 함수

--- src/main/java/com/practice/dockerConn/domain/article/ApiV1ArticleController.java
+++ src/main/java/com/practice/dockerConn/domain/article/ApiV1ArticleController.java
@@ -1,10 +1,12 @@
 package com.practice.dockerConn.domain.article;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -31,5 +33,15 @@ public class ApiV1ArticleController {
         }
         model.addAttribute("article", article);
         return "article_detail";
+    }
+
+    @GetMapping("/create")
+    public String createArticle () {
+        return "article_form";
+    }
+
+    @PostMapping("/create")
+    public String createArticle (Model model) {
+        return "article_list";
     }
 }
